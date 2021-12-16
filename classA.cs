a5091b51379df1a063d5d152397b61398802cb4b
Mohammed SaadEldin Elsuissey <mohammed.elsuissey@DeliveryHero.Egypt>
2021-12-16 13:09:39 +0200

    my class

---
+++ classA.cs
@@ -0,0 +1,21 @@
+using system;
+namespace Ayhaga
+{
+    public class classA
+    {
+        public classA()
+        {
+
+        }
+
+        void Method1()
+        {
+
+        }
+
+        void Method2()
+        {
+
+        }
+    }
+}
